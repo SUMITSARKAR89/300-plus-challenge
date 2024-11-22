c1ff4e2e2f3456168c7732cda2b185165e1eff61
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-11-22 18:51:56 +0600

    project 1: background color generate

--- project 1/background.js
+++ project 1/background.js
@@ -1,80 +1,80 @@
 // // ----------- project one (for change body part)---------- 
 // /*step=1*/
-// const btn = document.querySelector('#color-btn');
+const btn = document.querySelector('#color-btn');
 
-// function generateRandomCol (){
-//     const r = Math.floor(Math.random() *255);
-//     const g = Math.floor(Math.random() *255);
-//     const b = Math.floor(Math.random() *255);
+function generateRandomCol (){
+    const r = Math.floor(Math.random() *255);
+    const g = Math.floor(Math.random() *255);
+    const b = Math.floor(Math.random() *255);
 
-//     return `rgb(${r}, ${g}, ${b}`
+    return `rgb(${r}, ${g}, ${b}`
 
-// }
+}
 
-// btn.addEventListener("click", () => {
-//     document.body.style.backgroundColor = generateRandomCol()
-// } );
+btn.addEventListener("click", () => {
+    document.body.style.backgroundColor = generateRandomCol()
+} );
 
 // // ----------- project two (for change only selected part)----------
 // // steps 1 create onload function
 
-// const colorBtn = document.getElementById("color-btn");
-// const createColor = document.getElementById("create-color"); 
+const colorBtn = document.getElementById("color-btn");
+const createColor = document.getElementById("create-color"); 
 
-// window.onload = () =>{
-//     main();
-// }
+window.onload = () =>{
+    main();
+}
 
-// // steps 2 generate random color function
-// function generateRandomColor(){
-//     const r = Math.floor(Math.random() *255);
-//     const g = Math.floor(Math.random() *255);
-//     const b = Math.floor(Math.random() *255);
+// steps 2 generate random color function
+function generateRandomColor(){
+    const r = Math.floor(Math.random() *255);
+    const g = Math.floor(Math.random() *255);
+    const b = Math.floor(Math.random() *255);
 
-//     return `rgb(${r}, ${g}, ${b}`
-// }
+    return `rgb(${r}, ${g}, ${b}`
+}
 
 // // steps 3 collect all necessary referances
 
-// function main(){
+function main(){
 
-//     colorBtn.addEventListener("click", function(){
-//         let bgColor = generateRandomColor()
-//         createColor.style.backgroundColor = bgColor;
+    colorBtn.addEventListener("click", function(){
+        let bgColor = generateRandomColor()
+        createColor.style.backgroundColor = bgColor;
 
-//     })
-// }
+    })
+}
 
 
 
 // // ----------- project three (for mark the color vlaue)----------
-// window.onload = () =>{
-//     main()
-// }
+window.onload = () =>{
+    main()
+}
 
-// const colorBtn = document.getElementById("color-btn");
-// const createColor = document.getElementById("create-color"); 
-// const input = document.getElementById("input-option"); 
+const colorBtn = document.getElementById("color-btn");
+const createColor = document.getElementById("create-color"); 
+const input = document.getElementById("input-option"); 
 
 
-// function main(){
+function main(){
    
-//     colorBtn.addEventListener("click", function(){
-//         let bgColor = remdomGenerateHexColor();
-//         createColor.style.backgroundColor = bgColor;
-//         input.value = bgColor
-//     })
+    colorBtn.addEventListener("click", function(){
+        let bgColor = remdomGenerateHexColor();
+        createColor.style.backgroundColor = bgColor;
+        input.value = bgColor
+    })
     
 
-// }
+}
 
-// function remdomGenerateHexColor(){
-//     const r = Math.floor(Math.random() *255);
-//     const g = Math.floor(Math.random() *255);
-//     const b = Math.floor(Math.random() *255);
+function remdomGenerateHexColor(){
+    const r = Math.floor(Math.random() *255);
+    const g = Math.floor(Math.random() *255);
+    const b = Math.floor(Math.random() *255);
 
-//     return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
-// } 
+    return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+} 
 
 
 // ----------- project four (copy and paste the color code)----------
